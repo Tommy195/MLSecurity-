55855362c9c814f283071b3db673ee1de124f7f7
Tomi195 <tomasguagnini.iglesias@gmail.com>
2022-09-21 23:44:24 -0400

    a

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ mssql = {'host': 'dbhost',
          'db': 'db'}
 
 postgresql = {'host': '0.0.0.0',
-         'user': 'mercadolibreuser',
-         'passwd': 'merc4dol1bre!usr326778',
-         'db': 'mercadolibreapp'}
+         'user': 'your_db_usr',
+         'passwd': 'your_db_pwd',
+         'db': 'your_db_name'}
 
 
 mssqlConfig = "mssql+pyodbc://{}:{}@{}:1433/{}?driver=SQL+Server+Native+Client+10.0".format(mssql['user'], mssql['passwd'], mssql['host'], mssql['db'])
